4df87e31a6ef81470977c42cbd95ece949b27a2c
tran duy khanh <1822040474@dntu.edu.vn>
2024-09-07 10:14:39 +0700

    htbt05

---
+++ BT04/BT04/Controllers/TheLoaiController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace BT04.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Edit(int id)
+        {
+            return Ok("Id: " + id);
+        }
+
+        public IActionResult Detail(int id, string ten)
+        {
+            return Content(String.Format("id: {0};ten:{1}", id, ten));
+        }
+
+        public IActionResult Show(List<string> categories)
+        {
+            string content = "Danh sách Thể Loại: ";
+            foreach (var category in categories)
+            {
+                content = content + category + ", ";
+            }
+
+            // Trim the last comma and space
+            content = content.TrimEnd(',', ' ');
+
+            return Content(content);
+        }
+    }
+}
---
+++ Baitap05/Baitap05/Controllers/TheLoaiController.cs
@@ -0,0 +1,25 @@
+﻿using Baitap05.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Baitap05.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Ngay = "Ngay 28";
+            ViewBag.Thang = " Thang 02";
+            ViewData["Nam"] = "Nam 2030";
+            return View();
+        }
+        public IActionResult Index2()
+        {
+            var theloai = new TheLoaiViewModel
+            {
+                Id = 1,
+                Name = "Trinh Thám"
+            };
+            return View(theloai);
+        }
+    }
+}
