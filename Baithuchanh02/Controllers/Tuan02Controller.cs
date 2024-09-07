db2fbc8dab1b1e0424219fab15b7375a9cc1988c
tran duy khanh <1822040474@dntu.edu.vn>
2024-09-07 10:53:06 +0700

    bthtvn

---
+++ Baithuchanh02/Controllers/Tuan02Controller.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Baithuchanh02.Controllers
+{
+    public class Tuan02Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.HoTen = "Trần Duy Khánh";
+            ViewBag.MSSV = " 1822040474";
+            ViewBag.Nam = "Năm 3";
+            return View();
+        }
+    }
+}
---
+++ Baithuchanh03/Controllers/Maytinh.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Baithuchanh03.Controllers
+{
+    public class MaytinhController : Controller
+    {
+        public IActionResult Index(double a, double b, string pheptinh)
+        {
+            double ketQua = 0;
+            string thongBao = "";
+            switch (pheptinh)
+            {
+                case "cong":
+                    ketQua = a + b;
+                    break;
+                case "tru":
+                    ketQua = a - b;
+                    break;
+                case "nhan":
+                    ketQua = a * b;
+                    break;
+                case "chia":
+                    if (b != 0)
+                    {
+                        ketQua = a / b;
+                    }
+                    else
+                    {
+                        thongBao = "Không thể chia cho 0.";
+                    }
+                    break;
+                default:
+                    thongBao = "Phép tính không hợp lệ.";
+                    break;
+            }
+
+            if (string.IsNullOrEmpty(thongBao))
+            {
+                thongBao = $"Kết quả: {ketQua}";
+            }
+            ViewBag.KetQua = thongBao;
+            return View();
+        }
+    }
+}
