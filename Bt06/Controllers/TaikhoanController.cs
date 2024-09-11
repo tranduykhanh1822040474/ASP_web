a6d3ddd405320f715cbdb7cd901d565ca68fad2d
tran duy khanh <1822040474@dntu.edu.vn>
2024-09-11 15:12:26 +0700

    htbt06

---
+++ Bt06/Controllers/TaikhoanController.cs
@@ -0,0 +1,21 @@
+﻿using Bt06.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTap06.Controllers
+{
+    public class TaiKhoanController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult DangKy(TaiKhoanViewModel taikhoan)
+        {
+            if (taikhoan != null && taikhoan.Password != null && taikhoan.Password.Length > 0)
+            {
+                taikhoan.Password = taikhoan.Password + "_da_ma_hoa";
+            }
+            return View(taikhoan);
+        }
+    }
+}
---
+++ Bt06/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bt06.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
