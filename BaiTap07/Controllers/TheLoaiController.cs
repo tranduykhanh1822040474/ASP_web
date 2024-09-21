417e4ead17cab9022ba77b44fe3e9e213e2aa16b
tran duy khanh <1822040474@dntu.edu.vn>
2024-09-21 10:42:35 +0700

    htbt

---
+++ BaiTap07/Controllers/TheLoaiController.cs
@@ -0,0 +1,36 @@
+﻿using BaiTap07.Data;
+using BaiTap07.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTap07.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public TheLoaiController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var theloai=_db.TheLoai.ToList();
+            ViewBag.theloai = theloai;
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+           
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(TheLoai theLoai)
+        {
+            _db.TheLoai.Add(theLoai);
+            _db.SaveChanges();
+
+            return View();
+        }
+    }
+}
---
+++ BaiTap07/Models/TheLoai.cs
@@ -0,0 +1,10 @@
+﻿namespace BaiTap07.Models
+{
+    public class TheLoai
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+
+    }
+}
